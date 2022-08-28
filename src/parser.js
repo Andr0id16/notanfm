7422789474e91aaaf520b9324a1613debdeb6eb9
Andr0id16 <pracheth.thamankar@gmail.com>
2022-08-28 18:36:32 +0530

    Added prompt to MainWindow
    MainWindow input field's value goes parser
    
    Modified parser.js to correctly join spaces in tokens

--- src/parser.js
+++ src/parser.js
@@ -1,13 +1,15 @@
-// pattern = new RegExp(/[(\"*\")(\'*\')(^[^*']*[^*'])]/);
+prompt = document.getElementById("prompt");
+
 pattern = new RegExp(/[\(\"*\"\)\(\'*'\\)\([^\"\']*[^\"\']\)]/);
 
 class Lexer {
   constructor(text) {
     this.text = text;
     this.tokens = this.text.split(" ");
-    this.joinTokensWithSpaces(0);
+    this.joinTokensWithSpaces();
   }
-  joinTokensWithSpaces(index = 0) {
+  //joins tokens that are seperated by spaces but must be used as a single unit like filenames with spaces
+  joinTokensWithSpaces() {
     let i = 0;
     while (i < this.tokens.length) {
       let currentToken = this.tokens[i];
@@ -18,29 +20,22 @@ class Lexer {
       if ((so && eo) || (sd && ed) || (!(so || eo) && !(sd || ed))) {
         i++;
       } else {
-        this.tokens[i] += this.tokens[i + 1];
-        i++;
-        this.tokens.splice(i, 1);
+        this.tokens[i] += " " + this.tokens[i + 1];
+        this.tokens.splice(i + 1, 1);
       }
     }
   }
-
-  clear() {
-    this.text = null;
-    this.tokens = null;
-  }
 }
 
-const prompt = require("prompt-sync")({ sigint: true });
+// const prompt = require("prompt-sync")({ sigint: true });
 //With readline
 
-function terminal() {
-  while (true) {
-    const text = prompt("terminal> ");
-    const lexer = new Lexer(text);
-    console.log(lexer.tokens);
-    delete lexer;
-  }
-}
+prompt.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") parse(prompt.value);
+});
 
-terminal();
+function parse(text) {
+  const lexer = new Lexer(text);
+  console.log(lexer.tokens);
+  delete lexer;
+}
